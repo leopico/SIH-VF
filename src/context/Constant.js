412f4f33dc2437f688e68e7f7ce022a3bfda06f6
leopico <linkoko.peceng@gmail.com>
2023-09-09 21:46:59 +0630

    changed for contract

--- src/context/Constant.js
+++ src/context/Constant.js
@@ -1,4 +1,4 @@
-import Contract from "../blockchain/contract/VirtualForest.json"; //have to change
+import Contract from "../blockchain/contract/VF.json"; //have to change
 import MANTokenContract from "../blockchain/contract/MANToken.json";
 
 export const contractAddress = "0xA23214b28c71D2c53009829556360756b9535db0"; //have to change
